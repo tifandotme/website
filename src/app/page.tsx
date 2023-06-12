add255ab292fee0f9f6ef72087c37b26162ef104
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-06-12 20:28:18 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-end justify-between p-24">
-      <button className="bg-red-500">Submit</button>
+    <main className="flex min-h-screen flex-col justify-between p-24">
+      <button className="bg-red-500">asdasd</button>
     </main>
-  )
+  );
 }
