b4cfff74d244d00cda677ce3794d264fbb14f5d6
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-06-23 22:32:23 +0700

    update layout, page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import "@/styles/globals.css";
+import "@/app/globals.css";
 import { type Metadata } from "next";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
   title: {
@@ -20,13 +21,30 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       {/* <script
         async
         src="https://analytics.tifan.me/script.js"
         data-website-id="7edd9838-f991-48d3-aa41-c7fe0cd55d36"
       /> */}
-      <body className={inter.className}>{children}</body>
+      <body>
+        <div className="mx-auto max-w-2xl px-4 py-10">
+          <header className="mt-32 flex flex-col items-start">
+            <h1 className="mb-4 text-4xl font-extrabold">
+              <Link href="/">Tifan</Link>
+            </h1>
+            <p className="mb-4 text-lg">
+              Welcome to my online hideout where I share my insights on all
+              things web development.
+            </p>
+            <p className="text-lg">
+              Find me on: <a href="https://twitter.com/tifandotme">Twitter</a>{" "}
+              or <a href="https://github.com/tifandotme/">GitHub</a>
+            </p>
+          </header>
+          <main>{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
-export default function Home() {
+"use client";
+
+import { allPosts, type Post } from "contentlayer/generated";
+import { compareDesc, format, parseISO } from "date-fns";
+import Link from "next/link";
+
+function PostCard(post: Post) {
   return (
-    <main>
-      <h1>Homepage</h1>
-    </main>
+    <Link href={post.slug}>
+      <div className="mb-5 rounded-2xl border bg-gray-100 p-5 hover:bg-gray-200">
+        <h3 className="text-xl font-semibold">{post.title}</h3>
+        <time dateTime={post.date}>
+          {format(parseISO(post.date), "LLLL d, yyyy")}
+        </time>
+        <p>{post.description}</p>
+      </div>
+    </Link>
+  );
+}
+
+export default function Home() {
+  const posts = allPosts.sort((a, b) =>
+    compareDesc(new Date(a.date), new Date(b.date))
+  );
+
+  console.log(posts);
+
+  return (
+    <>
+      <div className="mt-16">
+        {posts.map((post, idx) => (
+          <PostCard key={idx} {...post} />
+        ))}
+      </div>
+    </>
   );
 }
