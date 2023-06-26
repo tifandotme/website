5e6448c94351a4a9d8e29a470d5aa1942ebb94bf
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-06-26 13:13:55 +0700

    refactor: getPost function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx";
+import { clsx, type ClassValue } from "clsx";
+import { allPosts, type Post } from "contentlayer/generated";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function getPost(slug: string): Post | undefined {
+  return allPosts.find((post) => post.slug === slug);
 }
