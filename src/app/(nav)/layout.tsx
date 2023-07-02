93f0a3827029eb27aba0f9d0f23b343d17f18e67
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-07-02 23:49:01 +0700

    refactor: add navigaton client component, edit root layout

--- src/app/(nav)/layout.tsx
+++ src/app/(nav)/layout.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+import { Navigation } from "@/components/navigation";
+
 export default function HomeLayout({
   children,
 }: {
@@ -7,23 +9,18 @@ export default function HomeLayout({
 }) {
   return (
     <>
-      <nav className="flow-root p-16">
+      <nav className="container flow-root p-6 pt-10 sm:p-10 lg:p-16 ">
         <Link
           href="/"
-          className="absolute select-none text-5xl font-semibold leading-9 text-gray-500 hover:text-black lg:fixed"
+          className="absolute select-none text-5xl font-semibold leading-9  text-gray-500 transition hover:text-gray-700 active:translate-y-0.5 lg:fixed"
         >
           T
         </Link>
-        <div className="float-right inline-flex gap-5 text-lg font-medium text-gray-600">
-          <Link href="/blog" className="px-2 py-1 hover:text-black">
-            Blog
-          </Link>
-          <Link href="/projects" className="px-2 py-1 hover:text-black">
-            Projects
-          </Link>
+        <div className="float-right inline-flex gap-5 text-lg font-medium text-gray-500">
+          <Navigation />
         </div>
       </nav>
-      {children}
+      <main className="mx-auto my-10 max-w-3xl p-3 sm:p-5">{children}</main>
     </>
   );
 }
---
+++ src/components/navigation.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+
+const navLinks = [
+  { name: "Blog", href: "/blog" },
+  { name: "Projects", href: "/projects" },
+] as const;
+
+export function Navigation() {
+  const pathname = usePathname();
+
+  return (
+    <>
+      {navLinks.map(({ name, href }) => (
+        <Link
+          key={href}
+          href={href}
+          className={cn(
+            "px-2 py-1 transition hover:text-gray-900 active:translate-y-0.5",
+            pathname === href && "font-semibold text-gray-900"
+          )}
+        >
+          {name}
+        </Link>
+      ))}
+    </>
+  );
+}
