38d8054372d06f446a0250d0c153a1543c048483
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-07-02 23:50:52 +0700

    feat: add introductory paragraphs, add external links

--- src/app/(nav)/page.tsx
+++ src/app/(nav)/page.tsx
@@ -1,29 +1,56 @@
-import { allPosts } from "contentlayer/generated";
+import Image from "next/image";
+import { FaGithub, FaNewspaper, FaTwitter } from "react-icons/fa6";
 
-import { PostCard } from "@/components/post-card";
+import potrait from "@/public/profile-picture.png";
+import { ExternalLink } from "@/components/external-link";
+import { PostList } from "@/components/post-list";
 
 export default function HomePage() {
-  const posts = allPosts.sort((a, b) =>
-    new Intl.Collator().compare(b.date, a.date)
-  );
-
   return (
-    <main className="mx-auto mt-20 max-w-3xl p-5 text-lg">
-      <header className="flex flex-col items-start">
-        <p className="mb-4">
-          Welcome to my <em>online hideout</em> where I share my insights on all
-          things web development.
+    <>
+      <header className="text-lg leading-8 text-gray-700">
+        <Image
+          className="float-right ml-4 mr-10 mt-2 hidden rounded-full bg-gradient-to-b from-gray-300 to-gray-400 p-[1px] grayscale hover:grayscale-0 sm:block"
+          src={potrait}
+          alt="Self Potrait"
+          width={120}
+          height={130}
+          placeholder="blur"
+        />
+        {/* prettier-ignore */}
+        <p>
+          Hi, I&apos;m <em><strong>Tifan</strong></em>, a software engineer based in Indonesia. I&apos;m currently delving deeper into the React ecosystem and exploring various aspects of web development—while writing what I discover throughout my learning journey.
         </p>
-        <p className="text-lg">
-          Find me on: <a href="https://twitter.com/tifandotme">Twitter</a> or{" "}
-          <a href="https://github.com/tifandotme/">GitHub</a>
+        {/* prettier-ignore */}
+        <p className="my-6">
+          My ambition is to create an <em>efficient</em> and <em>elegant</em> software that can have a positive impact on people&apos;s lives.
         </p>
+        {/* prettier-ignore */}
+        <p>
+          Outside of programming, I&apos;m also passionate about <em>science fiction</em> and <em>philosophy</em>.
+        </p>
+
+        <section className="mb-16 mt-9 flex w-full justify-around gap-3 sm:justify-center sm:gap-6">
+          <ExternalLink
+            icon={FaNewspaper}
+            href="https://tifan.me/resume.pdf"
+            text="Resume"
+          />
+          <ExternalLink
+            icon={FaGithub}
+            href="https://github.com/tifandotme"
+            text="GitHub"
+          />
+          <ExternalLink
+            icon={FaTwitter}
+            href="https://twitter.com/tifandotme"
+            text="Twitter"
+          />
+        </section>
       </header>
-      <div className="mt-16">
-        {posts.map((post, idx) => (
-          <PostCard key={idx} {...post} />
-        ))}
-      </div>
-    </main>
+      <PostList />
+    </>
   );
 }
+
+// TODO: track event click for umami
