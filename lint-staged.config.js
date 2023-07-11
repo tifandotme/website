0c1d8ed6cd8622cd9230e5aaf6e7b220eb83a1b5
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-07-11 07:53:47 +0700

    chore: change config to esm

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+const config = {
   "src/**/*.{js,jsx,ts,tsx}": ["eslint --max-warnings=0 --fix"],
   "*.{js,jsx,ts,tsx,json,yaml,mdx,css}": ["prettier --write"],
-};
+}
+
+export default config
