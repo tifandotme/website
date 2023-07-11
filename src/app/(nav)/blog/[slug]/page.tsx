14976e39377f14460e1c9e6b236d23ef5ceed9dd
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-07-11 08:08:24 +0700

    refactor: revamp blog page

--- src/app/(nav)/blog/[slug]/page.tsx
+++ src/app/(nav)/blog/[slug]/page.tsx
@@ -1,40 +1,55 @@
-import { type Metadata } from "next";
-import { notFound } from "next/navigation";
-import { allPosts } from "contentlayer/generated";
-import { getMDXComponent } from "next-contentlayer/hooks";
+import { type Metadata } from "next"
+import Image, { ImageProps } from "next/image"
+import { notFound } from "next/navigation"
+import { allPosts } from "contentlayer/generated"
+import { MDXComponents } from "mdx/types"
+import { useMDXComponent } from "next-contentlayer/hooks"
 
-import { getPost } from "@/lib/utils";
+import { getPost } from "@/lib/utils"
 
-//#region Static Generation
 export async function generateStaticParams() {
-  return allPosts.map((post) => ({ slug: post.slug }));
+  return allPosts.map((post) => ({ slug: post.slug }))
 }
 
 export async function generateMetadata({
   params,
 }: {
-  params: { slug: string };
+  params: { slug: string }
 }): Promise<Metadata> {
-  const post = getPost(params.slug);
+  const post = getPost(params.slug)
 
   return {
     title: post?.title,
     description: post?.description,
-  };
+  }
 }
-//#endregion
+
+const components: MDXComponents = {
+  Image: ({ alt, props }: { alt: string; props: ImageProps }) => (
+    <Image {...props} alt={alt} />
+  ),
+}
 
 export default function PostPage({ params }: { params: { slug: string } }) {
-  const post = getPost(params.slug);
+  const post = getPost(params.slug)
 
-  if (!post) notFound();
+  if (!post) notFound()
 
-  const Content = getMDXComponent(post.body.code);
+  const date = new Intl.DateTimeFormat("en-US", { dateStyle: "long" }).format(
+    new Date(post.date),
+  )
+
+  const Content = useMDXComponent(post.body.code, {
+    components,
+  })
 
   return (
-    <article>
-      <h1>{post.title}</h1>
-      <Content />
+    <article className="prose mx-auto max-w-screen-md border xl:-mt-24">
+      <header className="not-prose">
+        <p>{date}</p>
+        <h1 className="text-3xl font-bold">{post.title}</h1>
+      </header>
+      <Content components={components} />
     </article>
-  );
+  )
 }
