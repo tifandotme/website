9ed37ff62cdf3389c95030ae436f6f8fc4f058e8
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-07-16 12:10:52 +0700

    refactor: change styling

--- src/components/social-button.tsx
+++ src/components/social-button.tsx
@@ -11,15 +11,15 @@ export function SocialButton({ icon, text, ...props }: SocialButtonProps) {
 
   return (
     <a
-      className="group inline-flex flex-col items-center justify-center gap-2 rounded-lg border border-gray-300 px-6 py-4 transition hover:-translate-y-1 active:scale-95 sm:flex-row sm:gap-3 sm:px-5"
+      className="group inline-flex flex-col items-center justify-center gap-2 rounded-lg border px-6 py-4 hover:-translate-y-1 hover:transition active:scale-95 sm:flex-row sm:gap-3 sm:px-4"
       rel="noopener noreferrer"
       data-umami-event={text + " button"}
       {...props}
     >
-      <span className="group-hover:text-[hsl(var(--accent))]">
-        {iconElement}
+      <span className="group-hover:text-primary">{iconElement}</span>
+      <span className="select-none text-base font-medium leading-[1.1em]">
+        {text}
       </span>
-      <span className="text-base font-medium leading-[1.1em]">{text}</span>
     </a>
   )
 }
