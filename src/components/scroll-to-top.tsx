3e0e10f870d3a2ec653741bf0c7f76c0d759d357
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-07-16 22:55:41 +0700

    refactor: disable back to top button when user have not scrolled, add icon

--- src/components/scroll-to-top.tsx
+++ src/components/scroll-to-top.tsx
@@ -1,19 +1,31 @@
 "use client"
 
 import Link from "next/link"
+import cn from "clsx"
+import { BiUpArrowAlt } from "react-icons/bi"
+import { useWindowScroll } from "react-use"
 
 export function ScrollToTopButton({ path }: { path: string }) {
+  const { y } = useWindowScroll()
+
+  const isScrolled = y > window.innerHeight
+
   return (
-    // TODO: hide when at top
     <Link
       onClick={() => {
         window.scrollTo({ top: 0, behavior: "instant" })
       }}
-      className="text-sm font-medium text-muted transition hover:text-foreground active:translate-y-0.5"
+      className={cn(
+        "text-sm font-medium transition-transform hover:text-foreground active:translate-y-0.5",
+
+        isScrolled
+          ? "pointer-events-auto text-muted"
+          : "pointer-events-none text-[hsl(0,0%,75%)] dark:text-[hsl(0,0%,20%)]",
+      )}
       href={path}
       prefetch={false}
     >
-      Scroll to top <span aria-hidden="true">↑</span>
+      Back to top <BiUpArrowAlt className="ml-[1px] inline-block" size={21} />
     </Link>
   )
 }
