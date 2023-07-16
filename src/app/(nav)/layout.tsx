a63fdff9bcab112df7104074c5e9d7b675778036
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-07-16 12:22:19 +0700

    feat: add dark mode

--- src/app/(nav)/layout.tsx
+++ src/app/(nav)/layout.tsx
@@ -4,35 +4,60 @@ import React from "react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import cn from "clsx"
+import { allPosts } from "contentlayer/generated"
+import { useTheme } from "next-themes"
+import { CgDarkMode } from "react-icons/cg"
+import { HiOutlineDotsHorizontal } from "react-icons/hi"
 
 import { navLinks } from "@/config"
-import { Logo } from "@/components/logo"
+import { LogoSvg } from "@/components/logo"
 
 export default function HomeLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const [mounted, setMounted] = React.useState(false)
+  const { theme, setTheme } = useTheme()
+
   const ref = React.useRef<HTMLElement>(null)
+
   const pathname = usePathname()
+
+  // https://github.com/pacocoursey/next-themes#avoid-hydration-mismatch
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+  if (!mounted) return
+
+  const isBlogPage = allPosts.map((post) => post.url).includes(pathname)
 
   return (
     <>
       <nav
         ref={ref}
-        className="container flow-root px-6 py-16 sm:px-10 lg:px-16"
+        className="container flow-root px-6 py-14 sm:px-10 lg:px-16"
       >
         <Link
           href="/"
-          onClick={() => {
-            // TODO: is there a better way to scroll to top?
-            ref.current?.scrollIntoView({ behavior: "instant" })
-          }}
-          className="absolute select-none transition active:translate-y-0.5 lg:fixed"
+          // TODO: create tooltip component?
+          title="Go to homepage"
+          className="absolute z-10 select-none transition-transform active:translate-y-0.5 lg:fixed"
         >
-          <Logo intersectionRef={ref} />
+          <LogoSvg className="fill-muted-large text-[2.6rem] transition-colors hover:fill-foreground" />
         </Link>
-        <div className="float-right inline-flex gap-3">
+
+        <div className="group relative float-right inline-flex gap-3">
+          <div
+            className={cn(
+              "translate-x-60 p-2 text-muted-large group-hover:invisible",
+
+              isBlogPage ? "visible" : "invisible",
+            )}
+          >
+            <HiOutlineDotsHorizontal size={28} />
+          </div>
+
           {navLinks.map(({ title, url }) => {
             const isActive = pathname === url
 
@@ -41,14 +66,32 @@ export default function HomeLayout({
                 key={url}
                 href={url}
                 className={cn(
-                  "px-3 py-2 text-lg font-medium text-gray-400 underline transition hover:text-gray-700 active:translate-y-0.5",
-                  isActive && "text-gray-700",
+                  "select-none px-3 py-2 text-lg font-semibold text-muted-large transition hover:bg-[hsl(0,0%,93%)] hover:text-foreground active:translate-y-0.5 group-hover:visible dark:hover:bg-[hsl(0,0%,11%)]",
+
+                  isActive && "!text-foreground",
+                  isBlogPage && "invisible",
                 )}
               >
                 {title}
               </Link>
             )
           })}
+          <button
+            className={cn(
+              "px-3 text-muted-large transition-transform hover:bg-[hsl(0,0%,93%)] hover:text-foreground active:translate-y-0.5 group-hover:visible dark:hover:bg-[hsl(0,0%,11%)]",
+
+              isBlogPage && "invisible",
+            )}
+            onClick={() => {
+              setTheme(theme === "light" ? "dark" : "light")
+            }}
+            title={`Change to ${theme === "light" ? "dark" : "light"} mode`}
+          >
+            <CgDarkMode
+              className={cn(theme === "light" && "rotate-180")}
+              size={25}
+            />
+          </button>
         </div>
       </nav>
 
