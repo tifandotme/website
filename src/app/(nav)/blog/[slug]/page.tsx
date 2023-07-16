36633d993f7a58df8914dda155e4016e01022a61
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-07-16 12:21:45 +0700

    refactor: revamp blog page

--- src/app/(nav)/blog/[slug]/page.tsx
+++ src/app/(nav)/blog/[slug]/page.tsx
@@ -1,11 +1,14 @@
 import { type Metadata } from "next"
 import Image, { ImageProps } from "next/image"
 import { notFound } from "next/navigation"
+import cn from "clsx"
 import { allPosts } from "contentlayer/generated"
 import { MDXComponents } from "mdx/types"
 import { useMDXComponent } from "next-contentlayer/hooks"
 
+import { type HeadingsField } from "@/types"
 import { getPost } from "@/lib/utils"
+import { ScrollToTopButton } from "@/components/scroll-to-top"
 
 export async function generateStaticParams() {
   return allPosts.map((post) => ({ slug: post.slug }))
@@ -25,6 +28,7 @@ export async function generateMetadata({
 }
 
 const components: MDXComponents = {
+  // BUG: fix this
   Image: ({ alt, props }: { alt: string; props: ImageProps }) => (
     <Image {...props} alt={alt} />
   ),
@@ -44,12 +48,62 @@ export default function PostPage({ params }: { params: { slug: string } }) {
   })
 
   return (
-    <article className="prose mx-auto max-w-screen-md border xl:-mt-24">
-      <header className="not-prose">
-        <p>{date}</p>
-        <h1 className="text-3xl font-bold">{post.title}</h1>
-      </header>
-      <Content components={components} />
-    </article>
+    <main className="container px-3 duration-75 animate-in fade-in-60 sm:px-5">
+      <aside
+        className={cn(
+          "fixed bottom-14 hidden select-none flex-col gap-2.5 pl-11 xl:flex",
+
+          // element doesn't render instantly after page load, so I added animation to avoid it looking like trash :)
+          "duration-200 animate-in slide-in-from-left-1/2",
+        )}
+      >
+        <p className="font-mono text-sm font-bold uppercase text-muted-large">
+          On this page
+        </p>
+        {(post.headings as HeadingsField).map((heading) => (
+          <a
+            key={heading.slug}
+            href={`#${heading.slug}`}
+            className="block text-sm font-medium text-muted transition hover:text-foreground active:translate-y-0.5"
+          >
+            {heading.text}
+          </a>
+        ))}
+        <hr className="my-3" />
+        <ScrollToTopButton path={post.url} />
+      </aside>
+
+      {/* adjust 63 ch if font is changed from inter to something else */}
+      <article
+        className={cn(
+          "prose mx-auto !max-w-screen-md dark:prose-invert lg:-mt-24",
+
+          "grid grid-cols-[min(63ch,100%),1fr] [&>*]:col-span-full md:[&>*]:col-[1/auto]",
+        )}
+      >
+        <header
+          className={cn(
+            "not-prose !col-span-full mb-8 [&+h2]:mt-5",
+
+            "grid grid-cols-[min(63ch,100%),1fr] [&>*]:col-span-full md:[&>*]:col-[1/auto]",
+          )}
+        >
+          <time
+            dateTime={post.date}
+            className="font-mono font-medium leading-loose text-muted"
+          >
+            {date}
+          </time>
+          <h1 className="!col-span-full mt-5 font-serif text-5xl font-medium leading-none tracking-tighter text-[var(--heading)]">
+            {post.title}
+          </h1>
+          {post.description && (
+            <p className="mt-7 text-muted">{post.description}</p>
+          )}
+        </header>
+
+        <Content components={components} />
+      </article>
+    </main>
   )
 }
