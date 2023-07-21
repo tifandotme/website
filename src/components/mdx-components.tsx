f970353cb56a40c2190a4cdc2666cd216aaeef79
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-07-21 23:29:41 +0700

    feat: add components for MDXContent

---
+++ src/components/mdx-components.tsx
@@ -0,0 +1,30 @@
+import { ImageProps } from "next/image"
+import cn from "clsx"
+import { type MDXComponents } from "mdx/types"
+
+import { CldImage } from "@/components/image"
+
+// XXX: .mdx doesn't have intellisense
+export const components: MDXComponents = {
+  Img: ({
+    bleed = false,
+    caption,
+    ...props
+  }: {
+    aspectRatio?: "16:9" | "4:3" | "1:1"
+    bleed?: boolean
+    caption?: string
+  } & ImageProps) => {
+    return (
+      <figure className={cn(bleed ? "!col-span-full" : "justify-self-center")}>
+        <CldImage className="rounded-lg border" {...props} />
+
+        {caption && (
+          <figcaption className="text-center text-sm text-muted">
+            {caption}
+          </figcaption>
+        )}
+      </figure>
+    )
+  },
+}
