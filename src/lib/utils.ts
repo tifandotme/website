db4823c34c522ba7af6b32f114d609d556a14e5a
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-07-21 23:32:49 +0700

    refactor: add getImage, generateSignature, signImage util functions (MAJOR)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,64 @@
 import { allPosts, type Post } from "contentlayer/generated"
+import { getPlaiceholder } from "plaiceholder"
 
 export function getPost(slug: string): Post | undefined {
   return allPosts.find((post) => post.slug === slug)
+}
+
+/**
+ * Get the image height and base64-encoded placeholder for a given remote image URL.
+ */
+export async function getImage(src: string) {
+  const buffer = await fetch(src).then(async (res) =>
+    Buffer.from(await res.arrayBuffer()),
+  )
+
+  const {
+    metadata: { height },
+    base64,
+  } = await getPlaiceholder(buffer, { size: 10 })
+
+  return {
+    height,
+    base64,
+  }
+}
+
+/**
+ * Generate Cloudinary signature component for a given URL. Requires the `CLOUDINARY_API_SECRET` environment variable to be set.
+ *
+ * @param url - Cloudinary delivery URL without the base (e.g. w_300,h_250,e_grayscale/sample.png)
+ *
+ * @return Signature component for the URL (e.g. s--Kjba5B8k--).
+ *
+ * @see https://cloudinary.com/documentation/advanced_url_delivery_options#generating_delivery_url_signatures
+ */
+export async function generateSignature(url: string): Promise<string> {
+  const appendedUrl = url + process.env.CLOUDINARY_API_SECRET
+
+  // convert the data to UTF-8 encoded bytes
+  const data = new TextEncoder().encode(appendedUrl)
+
+  // generate SHA-256 hash of the data
+  const hash = await crypto.subtle.digest("SHA-256", data)
+
+  // convert the hash to a base64-encoded string
+  const base64 = Buffer.from(hash).toString("base64")
+
+  const urlSafe = base64
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=/g, "")
+
+  const formattedSignature = `s--${urlSafe.slice(0, 8)}--`
+
+  return formattedSignature
+}
+
+export async function signImage(transforms: string, publicId: string) {
+  const base = "https://res.cloudinary.com/tifan"
+
+  const signature = await generateSignature([transforms, publicId].join("/"))
+
+  return [base, signature, transforms, publicId].join("/")
 }
