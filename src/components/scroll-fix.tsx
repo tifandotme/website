28778361fa7658e3a4565c2b7b23de1958531084
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-07-23 12:20:12 +0700

    chore: instant scroll  on "scroll fix"

--- src/components/scroll-fix.tsx
+++ src/components/scroll-fix.tsx
@@ -3,12 +3,19 @@
 import React from "react"
 
 /**
- * next/link component will not scroll to top when navigating to a new page. This will fix that.
+ * next/link component will not scroll to top when navigating to a new page. This will scroll to top when the component is mounted.
  *
  * @see https://github.com/vercel/next.js/issues/42492
  */
 export function ScrollUpWhenMounted() {
-  React.useEffect(() => window.document.scrollingElement?.scrollTo(0, 0), [])
+  React.useEffect(
+    () =>
+      window.document.scrollingElement?.scrollTo({
+        top: 0,
+        behavior: "instant",
+      }),
+    [],
+  )
 
   return null
 }
