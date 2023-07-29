d3f99f7a07b59682741366f909f7f351f5f84053
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-07-29 21:27:16 +0700

    refactor: add font-heading

--- tailwind.config.js
+++ tailwind.config.js
@@ -86,8 +86,9 @@ const config = {
       fontFamily: {
         sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
         mono: ["var(--font-mono)", ...defaultTheme.fontFamily.mono],
-        serif: ["var(--font-serif)", ...defaultTheme.fontFamily.serif], // used in <em>
-        italic: ["var(--font-serif-slanted)", ...defaultTheme.fontFamily.serif],
+        serif: ["var(--font-serif)", ...defaultTheme.fontFamily.serif],
+        italic: ["var(--font-slanted)", ...defaultTheme.fontFamily.serif],
+        heading: ["var(--font-heading)", ...defaultTheme.fontFamily.serif],
       },
     },
   },
