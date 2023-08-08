c7013e64e9aab82482ca66a87d99e90a98f0028a
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-08-08 18:58:02 +0700

    refactor: add /drafts menu when in dev env

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,14 +6,22 @@ export const site = {
     "A blog and portfolio site by Tifan. Follow my journey in web development through insightful articles and inspiring projects.",
   author: "Tifan Dwi Avianto",
   baseUrl: "https://tifan.me",
-  navLinks: [
-    {
-      label: "Blog",
-      url: "/blog",
-    },
-    {
-      label: "Projects",
-      url: "/projects",
-    },
-  ] satisfies NavLinks,
+  navLinks: (
+    [
+      {
+        label: "Drafts",
+        url: "/drafts",
+      },
+      {
+        label: "Blog",
+        url: "/blog",
+      },
+      {
+        label: "Projects",
+        url: "/projects",
+      },
+    ] satisfies NavLinks
+  ).filter((link) =>
+    process.env.NODE_ENV === "production" ? link.url !== "/drafts" : true,
+  ),
 }
