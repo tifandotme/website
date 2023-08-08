869d9bc4c1876d287d9b6093aacb7d8534290d98
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-08-08 18:56:18 +0700

    refactor: remove custom cursor

--- src/components/social-button.tsx
+++ src/components/social-button.tsx
@@ -1,8 +1,6 @@
 import React from "react"
 import { type IconType } from "react-icons"
 
-import { cn } from "@/lib/utils"
-
 type SocialButtonProps = {
   icon: IconType
   label: string
@@ -13,11 +11,7 @@ export function SocialButton({ icon, label, ...props }: SocialButtonProps) {
 
   return (
     <a
-      className={cn(
-        "group inline-flex flex-col items-center justify-center gap-2 rounded-lg border px-6 py-4 hover:-translate-y-1 hover:transition active:scale-95 sm:flex-row sm:gap-3 sm:px-4",
-
-        props.target === "_blank" && "cursor-ne-resize",
-      )}
+      className="group inline-flex flex-col items-center justify-center gap-2 rounded-lg border px-6 py-4 hover:-translate-y-1 hover:transition active:scale-95 sm:flex-row sm:gap-3 sm:px-4"
       data-umami-event={label + " button"}
       {...props}
     >
