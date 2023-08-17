f7356b761a95756d014707957a95872243cf6dc4
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-08-17 15:20:48 +0700

    chore: add custom prose style

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,12 @@ const config = {
       ...defaultTheme.screens,
     },
     extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
+        mono: ["var(--font-mono)", ...defaultTheme.fontFamily.mono],
+        italic: ["var(--font-italic)", ...defaultTheme.fontFamily.serif],
+        heading: ["var(--font-heading)", ...defaultTheme.fontFamily.serif],
+      },
       fontWeight: {
         inherit: "inherit",
       },
@@ -39,60 +45,6 @@ const config = {
           DEFAULT: "hsl(var(--muted-small-text))",
           darker: "hsl(var(--muted-large-text))",
         },
-      },
-      typography: {
-        // default prose styles: ./node_modules/@tailwindcss/typography/src/styles.js
-        DEFAULT: {
-          css: {
-            // discard prose-pre styles
-            pre: null,
-            code: null,
-            "code::before": null,
-            "code::after": null,
-
-            // custom prose colors
-            "--tw-prose-body": "hsl(var(--foreground))",
-            "--tw-prose-headings": "hsl(var(--heading))",
-            "--tw-prose-lead": colors.zinc[600],
-            "--tw-prose-links": "hsl(var(--bold))",
-            "--tw-prose-bold": "hsl(var(--bold))",
-            "--tw-prose-counters": colors.zinc[500],
-            "--tw-prose-bullets": colors.zinc[300],
-            "--tw-prose-hr": "hsl(var(--border))",
-            "--tw-prose-quotes": colors.zinc[900],
-            "--tw-prose-quote-borders": colors.zinc[200],
-            "--tw-prose-captions": colors.zinc[500],
-            "--tw-prose-code": colors.zinc[900],
-            "--tw-prose-pre-code": colors.red[200],
-            "--tw-prose-pre-bg": "var(--codeblock)",
-            "--tw-prose-th-borders": colors.zinc[300],
-            "--tw-prose-td-borders": colors.zinc[200],
-
-            "--tw-prose-invert-body": "hsl(var(--foreground))",
-            "--tw-prose-invert-headings": "hsl(var(--heading))",
-            "--tw-prose-invert-lead": colors.zinc[400],
-            "--tw-prose-invert-links": "hsl(var(--bold))",
-            "--tw-prose-invert-bold": "hsl(var(--bold))",
-            "--tw-prose-invert-counters": colors.zinc[400],
-            "--tw-prose-invert-bullets": colors.zinc[600],
-            "--tw-prose-invert-hr": "hsl(var(--border))",
-            "--tw-prose-invert-quotes": colors.zinc[100],
-            "--tw-prose-invert-quote-borders": colors.zinc[700],
-            "--tw-prose-invert-captions": colors.zinc[400],
-            "--tw-prose-invert-code": colors.white,
-            "--tw-prose-invert-pre-code": colors.red[300],
-            "--tw-prose-invert-pre-bg": "var(--codeblock)",
-            "--tw-prose-invert-th-borders": colors.zinc[600],
-            "--tw-prose-invert-td-borders": colors.zinc[700],
-          },
-        },
-      },
-      fontFamily: {
-        sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
-        mono: ["var(--font-mono)", ...defaultTheme.fontFamily.mono],
-        // serif: ["var(--font-serif)", ...defaultTheme.fontFamily.serif],
-        italic: ["var(--font-italic)", ...defaultTheme.fontFamily.serif],
-        heading: ["var(--font-heading)", ...defaultTheme.fontFamily.serif],
       },
       keyframes: {
         slideDown: {
@@ -136,6 +88,88 @@ const config = {
           },
           "100%": {
             background: "hsl(var(--muted-small-text) / 0%)",
+          },
+        },
+      },
+      typography: {
+        // default prose styles: ./node_modules/@tailwindcss/typography/src/styles.js
+        DEFAULT: {
+          css: {
+            h2: {
+              marginTop: "1.7rem",
+              fontFamily: [
+                "var(--font-heading)",
+                ...defaultTheme.fontFamily.serif,
+              ].join(", "),
+              fontWeight: defaultTheme.fontWeight.medium,
+              fontSize: defaultTheme.fontSize["3xl"][0],
+              ...defaultTheme.fontSize["3xl"][1],
+            },
+            "h2::before": {
+              display: "block",
+              marginBottom: "1rem",
+              height: "3px",
+              width: "1.5rem",
+              background: "hsl(var(--foreground))",
+              content: "''",
+            },
+            h3: {
+              marginTop: "1.3rem",
+              fontWeight: defaultTheme.fontWeight.medium,
+              fontSize: defaultTheme.fontSize["2xl"][0],
+              ...defaultTheme.fontSize["2xl"][1],
+            },
+
+            pre: null,
+            code: {
+              color: "hsl(var(--bold))",
+              background: "hsl(var(--muted-small-text)/17%)",
+              padding: "0.125rem 0.25rem",
+            },
+            "code::before": null,
+            "code::after": null,
+
+            a: {
+              textUnderlineOffset: "2px",
+            },
+            "ol, ul": {
+              marginTop: "0",
+            },
+
+            // custom prose colors
+            "--tw-prose-body": "hsl(var(--foreground))",
+            "--tw-prose-headings": "hsl(var(--heading))",
+            "--tw-prose-lead": colors.zinc[600],
+            "--tw-prose-links": "hsl(var(--bold))",
+            "--tw-prose-bold": "hsl(var(--bold))",
+            "--tw-prose-counters": colors.zinc[500],
+            "--tw-prose-bullets": colors.zinc[300],
+            "--tw-prose-hr": "hsl(var(--border))",
+            "--tw-prose-quotes": colors.zinc[900],
+            "--tw-prose-quote-borders": colors.zinc[200],
+            "--tw-prose-captions": colors.zinc[500],
+            "--tw-prose-code": colors.zinc[900],
+            "--tw-prose-pre-code": colors.red[200],
+            "--tw-prose-pre-bg": "var(--codeblock)",
+            "--tw-prose-th-borders": colors.zinc[300],
+            "--tw-prose-td-borders": colors.zinc[200],
+
+            "--tw-prose-invert-body": "hsl(var(--foreground))",
+            "--tw-prose-invert-headings": "hsl(var(--heading))",
+            "--tw-prose-invert-lead": colors.zinc[400],
+            "--tw-prose-invert-links": "hsl(var(--bold))",
+            "--tw-prose-invert-bold": "hsl(var(--bold))",
+            "--tw-prose-invert-counters": colors.zinc[400],
+            "--tw-prose-invert-bullets": colors.zinc[600],
+            "--tw-prose-invert-hr": "hsl(var(--border))",
+            "--tw-prose-invert-quotes": colors.zinc[100],
+            "--tw-prose-invert-quote-borders": colors.zinc[700],
+            "--tw-prose-invert-captions": colors.zinc[400],
+            "--tw-prose-invert-code": colors.white,
+            "--tw-prose-invert-pre-code": colors.red[300],
+            "--tw-prose-invert-pre-bg": "var(--codeblock)",
+            "--tw-prose-invert-th-borders": colors.zinc[600],
+            "--tw-prose-invert-td-borders": colors.zinc[700],
           },
         },
       },
