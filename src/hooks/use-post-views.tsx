fd1b58b62bebb024436d730a2f3bd25d2aa8116a
Tifan <33323177+tifandotme@users.noreply.github.com>
2023-12-10 15:13:24 +0700

    refactor: revamp usepostviews hook

--- src/hooks/use-post-views.tsx
+++ src/hooks/use-post-views.tsx
@@ -1,29 +1,51 @@
 import useSWR from "swr"
 
-import { getViews, incrementViews } from "@/lib/utils"
+import type { Response } from "@/types"
+
+async function getViews(slug: string) {
+  const url = new URL(`/api/views/${slug}`, process.env.NEXT_PUBLIC_APP_URL)
+  const res = await fetch(url)
+
+  if (!res.ok) {
+    throw new Error(`Failed to fetch views (${slug})`)
+  }
+
+  const json: Response<number> = await res.json()
+  return json.data
+}
+
+async function incrementViews(slug: string) {
+  // prettier-ignore
+  const url = new URL(`/api/views/${slug}?incr`, process.env.NEXT_PUBLIC_APP_URL)
+  const res = await fetch(url)
+
+  if (!res.ok) {
+    throw new Error(`Failed to increment views (${slug})`)
+  }
+
+  const json: Response<number> = await res.json()
+  return json.data
+}
 
 export function usePostViews(slug: string) {
-  const { data, mutate } = useSWR(slug, getViews, {
+  const { data: views, mutate } = useSWR(slug, getViews, {
     revalidateOnFocus: false,
     revalidateOnMount: false,
 
-    // if enabled, data will not be undefined initially and isLoading will always be false
+    // Data will not be undefined initially and isLoading will always be false
     suspense: true,
   })
 
   const increment = () => {
     mutate(incrementViews(slug), {
-      optimisticData: {
-        // very rare cases where views is null (new post), fallback to 0
-        views: (data.views ?? 0) + 1,
-      },
+      optimisticData: (views ?? 0) + 1,
       revalidate: false,
       populateCache: true,
     })
   }
 
   return {
-    views: data.views,
+    views: views ?? 0,
     increment,
   }
 }
