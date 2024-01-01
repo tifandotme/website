a618549e96ab0c5a64163ace2a79d26c2799a454
Tifan <33323177+tifandotme@users.noreply.github.com>
2024-01-01 14:16:01 +0700

    refactor: remove `octokit/rest`

--- src/lib/github/init.ts
+++
@@ -1,9 +0,0 @@
-import { Octokit } from "@octokit/rest"
-
-// NOTE rate limit for authenticated user is 5000 req/hour
-// https://docs.github.com/en/rest/overview/rate-limits-for-the-rest-api?apiVersion=2022-11-28#primary-rate-limit-for-authenticated-users
-
-export const octokit = new Octokit({
-  auth: process.env.GITHUB_ACCESS_TOKEN,
-  userAgent: "tifandotme/website",
-})
