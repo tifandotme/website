67f2ce382c58a275fd2db8c28182bc10c4557058
Tifan <33323177+tifandotme@users.noreply.github.com>
2024-04-07 00:15:52 +0700

    refactor: update robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,11 @@
 import type { MetadataRoute } from "next"
 
-import { absoluteUrl } from "@/lib/utils"
-
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: absoluteUrl("/sitemap.xml"),
+    sitemap: "https://tifan.me/sitemap.xml",
   }
 }
