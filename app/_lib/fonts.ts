69fc93329e5f29497f975675935f05e543a079f2
Tifan <33323177+tifandotme@users.noreply.github.com>
2024-04-07 00:21:57 +0700

    refactor: replace heading font to Imbue

--- app/_lib/fonts.ts
+++ app/_lib/fonts.ts
@@ -1,17 +1,13 @@
-import {
-  Imbue,
-  JetBrains_Mono,
-  Newsreader,
-  Nunito_Sans,
-} from "next/font/google"
+import { Imbue, Newsreader, Nunito_Sans } from "next/font/google"
+import localFont from "next/font/local"
 
 const fontSans = Nunito_Sans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
 
-const fontMono = JetBrains_Mono({
-  subsets: ["latin"],
+const fontMono = localFont({
+  src: "./commitmono-v143.woff2", // relative to where localFont is invoked
   variable: "--font-mono",
 })
 
@@ -28,6 +24,10 @@ const fontItalic = Newsreader({
 const fontHeading = Imbue({
   subsets: ["latin"],
   variable: "--font-heading",
+
+  // REF temp fix from https://github.com/vercel/next.js/issues/47115#issuecomment-1807197912
+  display: "swap",
+  adjustFontFallback: false,
 })
 
 export const fonts = [fontSans, fontMono, fontItalic, fontHeading]
