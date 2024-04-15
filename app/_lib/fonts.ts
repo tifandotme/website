a43c8dbc4e7b76bb53023f9757a08749439da3de
Tifan <33323177+tifandotme@users.noreply.github.com>
2024-04-15 13:00:17 +0700

    refactor: remove temp fix in font fallback

--- app/_lib/fonts.ts
+++ app/_lib/fonts.ts
@@ -1,6 +1,8 @@
 import { Imbue, Newsreader, Nunito_Sans } from "next/font/google"
 import localFont from "next/font/local"
 
+// https://github.com/seek-oss/capsize/pull/192
+
 const fontSans = Nunito_Sans({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -8,21 +10,13 @@ const fontSans = Nunito_Sans({
 
 const fontItalic = Newsreader({
   subsets: ["latin"],
-  style: "italic",
   variable: "--font-italic",
-
-  // REF temp fix from https://github.com/vercel/next.js/issues/47115#issuecomment-1807197912
-  display: "swap",
-  adjustFontFallback: false,
+  style: "italic",
 })
 
 const fontHeading = Imbue({
   subsets: ["latin"],
   variable: "--font-heading",
-
-  // temp fix
-  display: "swap",
-  adjustFontFallback: false,
 })
 
 const Commit_Mono = localFont({
