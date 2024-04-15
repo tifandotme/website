01ba99af62da951c74dac04865655df0954b2fc5
Tifan <33323177+tifandotme@users.noreply.github.com>
2024-04-15 17:13:26 +0700

    refactor: use named transformation

--- app/loader.js
+++ app/loader.js
@@ -6,5 +6,5 @@
  * @param {number} options.width
  */
 export default function cloudinaryLoader({ src: publicId, width }) {
-  return `https://res.cloudinary.com/tifan/c_limit,w_${width},q_auto,f_webp/${publicId}`
+  return `https://res.cloudinary.com/tifan/$w_${width}/t_blog/${publicId}`
 }
