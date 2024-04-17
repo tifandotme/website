63363e0c47de2acf9c3143674e7b1fa9440a4d29
Tifan <33323177+tifandotme@users.noreply.github.com>
2024-04-17 14:48:11 +0700

    refactor: rename url

--- app/loader.js
+++ app/loader.js
@@ -6,5 +6,5 @@
  * @param {number} options.width
  */
 export default function cloudinaryLoader({ src: publicId, width }) {
-  return `https://res.cloudinary.com/tifan/$w_${width}/t_blog/${publicId}`
+  return `https://res.cloudinary.com/tifan/$w_${width}/t_auto/${publicId}`
 }
