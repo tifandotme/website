727cff27fce79724092ea2833e4175e74553dcce
Tifan Dwi Avianto <33323177+tifandotme@users.noreply.github.com>
2024-07-06 08:10:36 +0700

    chore: update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,14 @@
 // REF https://github.com/vercel/next.js/blob/canary/packages/eslint-config-next/index.js
 
-// https://github.com/vercel/next.js/pull/64141 (@9 dep plugins is being worked on)
-
 /** @type {import("eslint").Linter.Config} */
 const config = {
   extends: [
-    "eslint:recommended",
     "next/core-web-vitals",
+    "next/typescript",
     "plugin:tailwindcss/recommended",
     "prettier",
   ],
   rules: {
-    "prefer-const": "error",
-    "no-undef": "off",
-    "@next/next/no-html-link-for-pages": "off",
     "tailwindcss/classnames-order": "off",
     "tailwindcss/no-custom-classname": "off",
   },
