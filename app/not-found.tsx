942030b1a303875544eb0980e84f49bf4fa9b108
Tifan Dwi Avianto <33323177+tifandotme@users.noreply.github.com>
2024-10-27 22:14:20 +0700

    refactor: use Link component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next"
+import Link from "next/link"
 import { Icon } from "./_components/icon"
 
 export const metadata: Metadata = {
@@ -11,13 +12,13 @@ export default function NotFound() {
       <h1 className="text-xl font-semibold text-foreground">
         Sorry, this page isn&apos;t available
       </h1>
-      <a
+      <Link
         className="inline-flex flex-row items-center justify-center gap-3 p-2.5 text-muted hover:text-foreground"
         href="/"
       >
         <Icon id="arrow-left" className="size-4" aria-hidden="true" />
         Back to homepage
-      </a>
+      </Link>
     </main>
   )
 }
