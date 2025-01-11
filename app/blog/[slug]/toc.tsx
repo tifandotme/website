94096b139b12495e45e179b60f4f43a3f807be74
Tifan Dwi Avianto <33323177+tifandotme@users.noreply.github.com>
2025-01-11 21:15:16 +0700

    refactor: adjust duration

--- app/blog/[slug]/toc.tsx
+++ app/blog/[slug]/toc.tsx
@@ -27,7 +27,8 @@ export function TableOfContents({ headings }: TableOfContentsProps) {
         </DialogPrimitive.Trigger>
         <DialogPrimitive.Portal>
           <DialogPrimitive.Overlay className="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-background/70" />
-          <DialogPrimitive.Content className="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-1/2 top-1/2 z-50 grid w-full max-w-lg -translate-x-1/2 -translate-y-1/2 gap-7 border bg-background p-6 shadow-lg duration-100">
+          {/* match duration to .5s with vaul drawer */}
+          <DialogPrimitive.Content className="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-1/2 top-1/2 z-50 grid w-full max-w-lg -translate-x-1/2 -translate-y-1/2 gap-7 border bg-background p-6 shadow-lg duration-500">
             <DialogPrimitive.Title className="text-lg font-bold leading-none tracking-tight">
               Table of Contents
             </DialogPrimitive.Title>
