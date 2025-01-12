48b24c6ca6d19ae1d9c925c7d4c6691f88acc462
Tifan Dwi Avianto <33323177+tifandotme@users.noreply.github.com>
2025-01-12 22:08:30 +0700

    refactor: adjust bg

--- app/blog/[slug]/toc.tsx
+++ app/blog/[slug]/toc.tsx
@@ -48,7 +48,7 @@ export function TableOfContents({ headings }: TableOfContentsProps) {
           <Button onClick={() => setIsOpen(true)} />
         </DialogPrimitive.Trigger>
         <DialogPrimitive.Portal>
-          <DialogPrimitive.Overlay className="fixed inset-0 z-50 bg-background/70 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
+          <DialogPrimitive.Overlay className="fixed inset-0 z-50 bg-white/70 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 dark:bg-black/70" />
           {/* match duration to .5s with vaul drawer */}
           <DialogPrimitive.Content className="fixed left-1/2 top-1/2 z-50 flex max-h-[80vh] w-full max-w-lg -translate-x-1/2 -translate-y-1/2 flex-col gap-7 border bg-background p-6 shadow-lg duration-500 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]">
             <DialogPrimitive.Title className="text-lg font-bold leading-none tracking-tight">
@@ -91,23 +91,27 @@ export function TableOfContents({ headings }: TableOfContentsProps) {
         <Button onClick={() => setIsOpen(true)} />
       </DrawerPrimitive.Trigger>
       <DrawerPrimitive.Portal>
-        <DrawerPrimitive.Overlay className="fixed inset-0 z-50 bg-background/70" />
+        {/* TODO test light mode bg if implemented */}
+        <DrawerPrimitive.Overlay className="fixed inset-0 z-50 bg-white/70 dark:bg-black/70" />
         <DrawerPrimitive.Content
           className={cn(
             "fixed inset-x-0 bottom-0 z-50 flex h-auto max-h-[50vh] flex-col rounded-t-3xl border-t-2 bg-background pb-7",
 
             position.y > 0 &&
-              "!bg-gradient-to-t from-background from-80% to-muted/5 to-95%",
+              "!bg-gradient-to-t from-background from-80% to-muted/10 to-95%",
           )}
         >
-          <div className="mx-auto my-3 h-2 w-[80px] shrink-0 rounded-full bg-muted-darker/30" />
+          <div
+            className={cn(
+              "mx-auto my-3 h-1.5 w-[80px] shrink-0 rounded-full bg-muted-darker/30",
+
+              position.y > 0 && "bg-muted-darker/50",
+            )}
+          />
           <DrawerPrimitive.Title className="mb-7 text-center text-xl font-bold">
             Table of Contents
           </DrawerPrimitive.Title>
-          <ul
-            ref={drawerScrollableEl}
-            className="overflow-y-auto bg-background px-3"
-          >
+          <ul ref={drawerScrollableEl} className="overflow-y-auto px-3">
             {headings.map((heading) => (
               <li key={heading.slug}>
                 <a
